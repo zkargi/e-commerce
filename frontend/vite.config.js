6283218c682b92126fb278ddada19247877d9b8d
Zeynep Kargı <zeynepkargi02@gmail.com>
2024-09-04 12:33:04 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: './frontend',  // index.html'in bulunduğu dizin
   plugins: [react()],
   server:{
-    port:3000,
+    port: 3000,
   },
   build: {
     outDir: 'dist'
